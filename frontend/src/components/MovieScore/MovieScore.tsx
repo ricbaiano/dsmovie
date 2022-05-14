5decc3b4565ba69f139c8ba768b4f9dffc4796dd
ricbaiano <ricbaiano@gmail.com>
2022-05-14 20:06:50 -0300

    Show Score

--- frontend/src/components/MovieScore/MovieScore.tsx
+++ frontend/src/components/MovieScore/MovieScore.tsx
@@ -1,15 +1,17 @@
 import MovieStar from "components/MovieStar";
 import "./styles.css";
 
-export default function MovieScore(){
+type Props = {
+    score: number;
+    count: number;
+}
 
-    const score = 3.5;
-    const count = 12;
+export default function MovieScore( { score, count } : Props){
 
     return(
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStar />
+            <MovieStar score={score}/>
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     )
